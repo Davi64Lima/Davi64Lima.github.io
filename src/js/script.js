a8925007613b21b6e10147b03942a52c8517b889
Davi Lima <novodavilima@gmail.com>
2023-07-02 11:45:37 -0300

    feat: Alterações por Liliane

--- src/js/script.js
+++ src/js/script.js
@@ -48,3 +48,54 @@ prevBtn.addEventListener("click", previousSlide);
 nextBtn.addEventListener("click", nextSlide);
 
 setInterval(nextSlide, 10000); // Change slide every 10 seconds
+
+/* INTERCAMBIO*/
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn");
+const popupInter = document.getElementById("popupInter");
+
+openBtn.addEventListener("click", function () {
+  popupInter.showModal();
+});
+
+closeBtn.addEventListener("click", function () {
+  popupInter.close();
+});
+
+/* PROFICE*/
+const opBtn = document.getElementById("opBtn");
+const clBtn = document.getElementById("clBtn");
+const popupDialog = document.getElementById("popupProfici");
+
+opBtn.addEventListener("click", function () {
+  popupDialog.showModal();
+});
+
+clBtn.addEventListener("click", function () {
+  popupDialog.close();
+});
+
+/* RU*/
+const opBton = document.getElementById("opBton");
+const clBton = document.getElementById("clBton");
+const popupRu = document.getElementById("popupRu");
+
+opBton.addEventListener("click", function () {
+  popupRu.showModal();
+});
+
+clBton.addEventListener("click", function () {
+  popupRu.close();
+});
+/*PROAE*/
+const opBt = document.getElementById("opBt");
+const clsBoton = document.getElementById("clsBoton");
+const popupProae = document.getElementById("popupProae");
+
+opBt.addEventListener("click", function () {
+  popupProae.showModal();
+});
+
+clsBoton.addEventListener("click", function () {
+  popupProae.close();
+});
